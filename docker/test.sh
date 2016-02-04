5fc78cef60dd8cd0a39fb244c84343a2177b16d7
pyama86 <pyama@pepabo.com>
2016-02-04 09:05:03 +0900

    update test

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,6 @@
-eval $(docker-machine env dev)
-docker build -f docker/ubuntu -t ubuntu:stns_test .
-docker build -f docker/rhel -t centos:stns_test .
+eval $(docker-machine env dev)  
+docker build --rm -f docker/ubuntu -t ubuntu:stns_test .
+docker build --rm -f docker/rhel -t centos:stns_test .
 
 docker run -t ubuntu:stns_test
 docker run -t centos:stns_test
