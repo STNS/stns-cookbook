37e2434b18a1cf9c8febb979726f749b4f49cbd7
pyama86 <pyama@pepabo.com>
2016-02-04 14:37:58 +0900

    update attribute nscd true to yes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,13 +8,13 @@ default['stns']['client']['api_end_point'] = 'http://localhost:1104'
 default['stns']['client']['chain_ssh_wrapper'] = nil
 
 %w(passwd group).each do |s|
-  default['nscd'][s]['enable_cache'] = true
+  default['nscd'][s]['enable_cache'] = 'yes'
   default['nscd'][s]['positive_time_to_live'] = 600
   default['nscd'][s]['negative_time_to_live'] = 300
-  default['nscd'][s]['check_files'] = true
-  default['nscd'][s]['shared'] = true
+  default['nscd'][s]['check_files'] = 'yes'
+  default['nscd'][s]['shared'] = 'yes'
 end
 
 %w(hosts services netgroup).each do |s|
-  default['nscd'][s]['enable_cache'] = false
+  default['nscd'][s]['enable_cache'] = 'no'
 end
