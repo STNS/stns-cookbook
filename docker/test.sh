6d06e1fcf2cdebec27ed1d579b2dda4ac9cec07b
pyama86 <pyama@pepabo.com>
2016-02-06 00:31:26 +0900

    not used cache of test

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,6 @@
 eval $(docker-machine env dev)  
-docker build --rm -f docker/ubuntu -t ubuntu:stns_test .
-docker build --rm -f docker/rhel -t centos:stns_test .
+docker build --rm --no-cache -f docker/ubuntu -t ubuntu:stns_test .
+docker build --rm --no-cache -f docker/rhel -t centos:stns_test .
 
 docker run -t ubuntu:stns_test
 docker run -t centos:stns_test
