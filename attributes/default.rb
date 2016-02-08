81e21755ce0819011149b1e6a86fd434db21ab69
pyama86 <pyama@pepabo.com>
2016-02-08 18:58:31 +0900

    support ssl verify

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,7 @@ default['stns']['server']['groups'] = [{}]
 
 default['stns']['client']['api_end_point'] = ['http://localhost:1104']
 default['stns']['client']['chain_ssh_wrapper'] = nil
+default['stns']['client']['ssl_verify'] = true
 
 %w(passwd group).each do |s|
   default['nscd'][s]['enable_cache'] = 'yes'
