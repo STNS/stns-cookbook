5b155999fe174ea761d6707d4d6c31cbb9960119
pyama86 <pyama@pepabo.com>
2016-02-10 20:45:21 +0900

    update nscd config

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,10 +14,11 @@ default['stns']['client']['ssl_verify'] = true
 
 %w(passwd group).each do |s|
   default['nscd'][s]['enable_cache'] = 'yes'
-  default['nscd'][s]['positive_time_to_live'] = 600
-  default['nscd'][s]['negative_time_to_live'] = 300
+  default['nscd'][s]['positive_time_to_live'] = 300
+  default['nscd'][s]['negative_time_to_live'] = 180
   default['nscd'][s]['check_files'] = 'yes'
   default['nscd'][s]['shared'] = 'yes'
+  default['nscd'][s]['persistent'] = 'no'
 end
 
 %w(hosts services netgroup).each do |s|
