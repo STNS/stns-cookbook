a1cc418cbe1d66ac35b3541d4fbff129360a5927
pyama86 <pyama@pepabo.com>
2016-02-14 12:00:08 +0900

    fix success test chain

--- docker/test.sh
+++ docker/test.sh
@@ -2,5 +2,4 @@ eval $(docker-machine env dev)
 docker build --rm --no-cache -f docker/ubuntu -t ubuntu:stns_test .
 docker build --rm --no-cache -f docker/rhel -t centos:stns_test .
 
-docker run -t ubuntu:stns_test
-docker run -t centos:stns_test
+docker run -t ubuntu:stns_test && docker run -t centos:stns_test
